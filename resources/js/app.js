c22c07492e72e36cc196c00545280a4595f00605
andriiF <andreiif123@gmail.com>
2023-03-14 21:50:20 +0100

    fortify logowanie + vue js 3 router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import ('./bootstrap')
+
+import {createApp} from 'vue/dist/vue.esm-bundler.js';
+import router from './router'
+import App from "./App.vue";
+
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
